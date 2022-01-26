b8a8bd5a798be0ad69ce804e7160c80611af5f27
Keoki <keoki.alan.mundon@gmail.com>
2022-01-26 14:57:32 -0800

    updated index.js

--- index.js
+++ index.js
@@ -1,44 +1,15 @@
-function newImage(name, left , bottom) {
+function newImage(url, left, bottom) {
     let image = document.createElement('img')
-    switch (name) {
-        case 'greenCharacter':
-            image.src = 'assets/green-character.gif'
-        break;
-        case 'pineTree':
-            image.src = 'assets/pine-tree.png'
-        break;
-        case 'tree':
-            image.src = 'assets/tree.png'
-        break;
-        case 'pillar':
-            image.src = 'assets/pillar.png'
-        break;
-        case 'crate':
-            image.src = 'assets/crate.png'
-        break;
-        case 'well':
-            image.src = 'assets/well.png'
-        break;
-    }
+    image.src = url
     image.style.position = 'fixed'
     image.style.left = left
     image.style.bottom = bottom
     document.body.append(image)
 }
 
-function newItem(name, left, bottom) {
+function newItem(url, left, bottom) {
     let image = document.createElement('img')
-    switch (name) {
-        case 'sword':
-            image.src = 'assets/sword.png'
-        break;
-        case 'shield':
-            image.src = 'assets/shield.png'
-        break;
-        case 'staff':
-            image.src = 'assets/staff.png'
-        break;
-    }
+    image.src = url
     image.style.position = 'fixed'
     image.style.left = left
     image.style.bottom = bottom
@@ -49,28 +20,28 @@ function newItem(name, left, bottom) {
 }
 
 // character
-newImage('greenCharacter', '100px', '100px')
+newImage('assets/green-character.gif', '100px', '100px')
 
 // pine tree
-newImage('pineTree', '450px', '200px')
+newImage('assets/pine-tree.png', '450px', '200px')
 
 // tree
-newImage('tree', '200px', '300px')
+newImage('assets/tree.png', '200px', '300px')
 
 // pillar
-newImage('pillar', '350px', '100px')
+newImage('assets/pillar.png', '350px', '100px')
 
 // crate
-newImage('crate', '150px', '200px')
+newImage('assets/crate.png', '150px', '200px')
 
 // well
-newImage('well', '500px', '425px')
+newImage('assets/well.png', '500px', '425px')
 
 // sword
-newItem('sword', '500px', '405px')
+newItem('assets/sword.png', '500px', '405px')
 
 // shield
-newItem('shield', '165px', '185px')
+newItem('assets/shield.png', '165px', '185px')
 
 // staff
-newItem('staff', '600px', '100px')
+newItem('assets/staff.png', '600px', '100px')
